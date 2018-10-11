2ed7da255bffc98774b1700d4bf8829eac10a7cb
alce65 <alce65@hotmail.es>
2018-10-11 18:24:38 +0200

    Componentes Bootstrap

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgbModule,
     AboutRoutingModule
   ],
   declarations: [AboutComponent]
